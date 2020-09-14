b36ce26fe3f0e21b9951d7be85bd463a7be55338
Multarix <darien2105@gmail.com>
2020-09-15 02:10:08 +1000

    Minor changes for cleaner code

--- events/me/change_zone.js
+++ events/me/change_zone.js
@@ -1,26 +1,26 @@
 const path = require("path");
 /* eslint-disable */
-	const sp_guides = [
-		3026, // Corrupted Skynest
-		3126, // Corrupted Skynest (Hard)
-		9050, // Rift's Edge (Hard)
-		9054, // Bathysmal Rise (Hard)
-		9044, // Bahaar's Sanctum
-		9066, // Demon's Wheel
-		9070, // Manglemire
-		9750, // Rift's Edge
-		9754, // Bathysmal Rise
-		9781, // Velik's Sanctuary
-		9916, // Sky Cruiser Endeavor (Hard)
-		9920, // Antaroth's Abyss (Hard)
-		9970, // Ruinous Manor (Hard)
-		9981 // Velik's Sanctuary (Hard)
-	];
-	const es_guides = [
-		3023, // Akalath Quarantine
-		9000, // ???
-		9759 // Forsaken Island (Hard)
-	];
+const sp_guides = [
+	3026, // Corrupted Skynest
+	3126, // Corrupted Skynest (Hard)
+	9050, // Rift's Edge (Hard)
+	9054, // Bathysmal Rise (Hard)
+	9044, // Bahaar's Sanctum
+	9066, // Demon's Wheel
+	9070, // Manglemire
+	9750, // Rift's Edge
+	9754, // Bathysmal Rise
+	9781, // Velik's Sanctuary
+	9916, // Sky Cruiser Endeavor (Hard)
+	9920, // Antaroth's Abyss (Hard)
+	9970, // Ruinous Manor (Hard)
+	9981 // Velik's Sanctuary (Hard)
+];
+const es_guides = [
+	3023, // Akalath Quarantine
+	9000, // ???
+	9759 // Forsaken Island (Hard)
+];
 	/* eslint-enable */
 
 module.exports = (mod, extras, zone, quick) => {
@@ -28,17 +28,10 @@ module.exports = (mod, extras, zone, quick) => {
 	mod.clearAllTimeouts();
 	mod.clearAllIntervals();
 
-	if(extras.guides.includes(extras.lastLocation.toString())){ // if the last location is the same as a guide, try unloading it
-		try {
-			extras.active_guide = false;
-			// delete require.cache[require.resolve(path.resolve(__dirname, `./guides/${zone}.js`))];
-		} catch (e){ null; }
-	}
+	if(extras.guides.includes(extras.lastLocation.toString())) extras.active_guide = false;
 
 	extras.lastLocation = zone; // Set the last location to this current location
 	if(!extras.guides.includes(zone.toString())) return extras.active_guide = false; // if the zone is not a dungeon, return
-
-	if(!mod.settings.enabled) return; // return if the guide isn't even enabled
 
 	let dungeon;
 	for(const d of mod.settings.dungeons){
