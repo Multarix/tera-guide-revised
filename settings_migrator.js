b257aa6f36d5dcbf6917f6501bfcd9626fe23ace
Multarix <darien2105@gmail.com>
2020-09-26 21:37:25 +1000

    update migrator Unlikely I'll need to actually use this file

--- settings_migrator.js
+++ settings_migrator.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const DefaultSettings = {
 	"enabled": true,
 	"notice": false,
@@ -10,36 +8,27 @@ const DefaultSettings = {
 };
 
 module.exports = function MigrateSettings(from_ver, to_ver, settings){
-	if(from_ver === undefined){
-		// Migrate legacy config file
-		return Object.assign(Object.assign({}, DefaultSettings), settings);
-	} else if(from_ver === null){
-		// No config file exists, use default settings
+	// We have no legacy config file, so just give a default settings file
+	if(!from_ver){
 		return DefaultSettings;
 	} else {
 		// Migrate from older version (using the new system) to latest one
 		if(from_ver + 1 < to_ver){
 			// Recursively upgrade in one-version steps
-			settings = MigrateSettings(from_ver, from_ver + 1, settings);
-
-			setTimeout(function(){
-				return MigrateSettings(from_ver + 1, to_ver, settings);
-			}, 0);
+			settings = MigrateSettings(from_ver, from_ver + 0.1, settings);
+			return MigrateSettings(from_ver + 0.1, to_ver, settings);
 		}
+
 		// If we reach this point it's guaranteed that from_ver === to_ver - 1, so we can implement
 		// a switch for each version step that upgrades to the next version. This enables us to
 		// upgrade from any version to the latest version without additional effort!
+		const oldSettings = settings;
+		settings = Object.assign(DefaultSettings, {});
 		switch(to_ver){
-			default:
-				let oldsettings = settings; //eslint-disable-line
-				settings = Object.assign(DefaultSettings, {});
-				for(const option in settings){
-					if(option == "lNotice" && oldsettings["notice"]) settings[option] = oldsettings["notice"];
-					if(option == "gNotice" && oldsettings["systemNotice"]) settings[option] = oldsettings["systemNotice"];
-				}
-				for(const option in oldsettings){
-					if(settings[option]){
-						settings[option] = oldsettings[option];
+			default: //
+				for(const value in oldSettings){
+					if(settings[value]){
+						settings[value] = oldSettings[value];
 					}
 				}
 				break;
