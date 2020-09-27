199f629210772813a8216ff8f5e22e1fe007f803
Multarix <darien2105@gmail.com>
2020-09-27 23:36:25 +1000

    Getting module ready for proper release

--- settings_migrator.js
+++ settings_migrator.js
@@ -7,32 +7,58 @@ const DefaultSettings = {
 	"dungeons": {}
 };
 
-module.exports = function MigrateSettings(from_ver, to_ver, settings){
-	// We have no legacy config file, so just give a default settings file
-	if(!from_ver){
+module.exports = function MigrateSettings(oldVersion, targetVersion, settings){
+	if(!oldVersion){ // We have no config file, and legeacy config file checking is pointless for this module, so just give a default settings file if the old version doesn't exist
 		return DefaultSettings;
 	} else {
-		// Migrate from older version (using the new system) to latest one
-		if(from_ver + 1 < to_ver){
-			// Recursively upgrade in one-version steps
-			settings = MigrateSettings(from_ver, from_ver + 0.1, settings);
-			return MigrateSettings(from_ver + 0.1, to_ver, settings);
-		}
+		oldVersion = parseFloat(oldVersion);
+		targetVersion = parseFloat(targetVersion);
 
-		// If we reach this point it's guaranteed that from_ver === to_ver - 1, so we can implement
-		// a switch for each version step that upgrades to the next version. This enables us to
-		// upgrade from any version to the latest version without additional effort!
-		const oldSettings = settings;
-		settings = Object.assign(DefaultSettings, {});
-		switch(to_ver){
-			default: //
-				for(const value in oldSettings){
-					if(settings[value]){
-						settings[value] = oldSettings[value];
-					}
+		if(oldVersion < targetVersion){ // If the old version is smaller than the target version
+			oldVersion += 0.1;	// Add 0.1 to the old version so we know which version we're upgrading to
+
+			const oldSettings = settings; // This is our old settings that we had
+			settings = Object.assign(DefaultSettings, {}); // This is our new settings
+
+			/* Seeing as there has been no settings upgrades, this part is commented out for now, but remains as an example
+			switch(oldVersion){
+				// We can simply make use of case fallthroughs for instant upgrades without need for repetition by removing breaks,
+				// simply setting oldVersion to the current highest version at the end... I swear most js module devs don't know about this
+				case 1.2:
+					console.log("Example Config version 1.2");
+					// Upgrade config from 1.1 to 1.2 here
+					oldVersion = 1.2;
+
+					// falls through
+				case 1.3:
+					console.log("Example Config version 1.3");
+					// Upgrade config from 1.2 to 1.3 here
+					oldVersion = 1.3;
+
+					// falls through
+				case 1.4:
+					console.log("Example Config version 1.4");
+					// Upgrade config from 1.3 to 1.4 here
+					oldVersion = 1.4;
+
+					// falls through
+				case 1.5:
+					console.log("Example Config version 1.5");
+					// Upgrade config from 1.4 to 1.5 here
+					oldVersion = 1.5;
+					break;
+				default: break;
+			}
+			*/
+
+			for(const key of oldSettings){ // If any of the keys in the old settings are the same as the keys in the new settings, apply them to the new settings
+				if(settings[key]){
+					settings[key] = oldSettings[key];
 				}
-				break;
+			}
+
+			settings = MigrateSettings(oldVersion, targetVersion, settings); // Loop through the function again, this will stop when oldVersion >= targetVersion, and thus will return settings as the most upgraded version available
 		}
-		return settings;
+		return settings; // Once we reach here, we know that oldVersion >= targetVersion and should now be ready for use!
 	}
 };
