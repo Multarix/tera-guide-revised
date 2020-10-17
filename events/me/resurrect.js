441df21ab6582be7dacac85cda4e1b123397c517
Multarix <darien2105@gmail.com>
2020-10-17 12:49:39 +1000

    fix name tho nothing uses this yet so it's not like it matters

--- events/me/resurrect.js
+++ events/me/resurrect.js
@@ -2,6 +2,6 @@ module.exports = (mod, extras) => {
 	if(!mod.settings.enabled || !extras.active_guide || !extras.verbose) return;
 	const { player } = mod.require.library;
 
-	const eventKey = `ressurect`;
+	const eventKey = `resurrect`;
 	return extras.eventHandler(mod, extras, { event: eventKey, target: false, ent: player, color: "#ff2121" });
 };
